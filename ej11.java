a0f2895fad6bbeeee5acca9f86fcd2b7629f6fe3
developer <cordero.velito.27@gmail.com>
2026-04-16 23:02:03 -0500

    ej11 solution

---
+++ ej11.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1, max = 0;
+        while (l < r) {
+            int h = Math.min(height[l], height[r]);
+            int w = r - l;
+            max = Math.max(max, h * w);
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+        return max;
+    }
+}
